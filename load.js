683098534d4cbed368de204058b12a21d2b1b245
Your Name <you@example.com>
2021-09-06 10:24:43 +0000

    first commit

---
+++ load.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import { check, fail } from "k6";
+
+export let options = {
+  stages: [
+    { duration: "30s", target: 1 },
+    { duration: "60s", target: 5 },
+    { duration: "90s", target: 10 },
+    { duration: "60m", target: 10 },
+  ]
+};
+
+export default function() {
+    let res1 = http.get(`${__ENV.TARGET}`);
+
+    check(res1, {
+        "Status 200": (r) => r.status === 200
+    });
+};
